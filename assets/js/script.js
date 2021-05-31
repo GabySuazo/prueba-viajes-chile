d8ce0a34cc8118ee2a2c10f750cbbb375d5e0511
Gabriela Suazo Egaña <gaby.suazo@gmail.com>
2021-05-31 01:55:05 -0400

    nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,16 @@ $(function() {
     $(".title").click(function(){
         $(".card-text").toggle();
         });
-
+    
+        $("a").click(function(event) {
+            if (this.hash !== "") {
+                var gato = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(gato).offset().top
+                    }, 800, function(){
+                        window.location.hash = gato;
+                    });
+            } 
+        });
         
 });
