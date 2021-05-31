d490ebf266e8a2baa1a41e513469844c5aac7c81
Gabriela Suazo Egaña <gaby.suazo@gmail.com>
2021-05-31 16:24:19 -0400

    scroll smooth

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(function() {
             if (this.hash !== "") {
                 var gato = this.hash;
                 $('html, body').animate({
-                    scrollTo: $(gato).offset().top
+                    scrollTop: $(gato).offset().top
                     }, 800, function(){
                         window.location.hash = gato;
                     });
