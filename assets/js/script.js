2a80e074e08b6b04adef1c14bd8d730ffd3c4e73
Gabriela Suazo Egaña <gaby.suazo@gmail.com>
2021-05-31 01:42:15 -0400

    carousel slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,5 @@ $(function() {
         $(".card-text").toggle();
         });
 
-        $("nav-link").click(function(event){
-            if (this.hash !== "") {
-                event.preventDefault();
-    
-                var gato = this.hash;
-    
-                $("html, body").animate({
-                    scrollTop: $(gato).offset().top
-                }, 600, function(){
-                    window.location.hash = gato;
-                });
-            }
-        });
-
+        
 });
-
